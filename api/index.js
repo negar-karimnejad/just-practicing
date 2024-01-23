df8741242ec7bc149386be121d8e4201beffd8ae
negar <negar.karimnejad@gmail.com>
2024-01-23 16:38:00 +0330

    coding

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 mongoose
@@ -24,3 +25,11 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
