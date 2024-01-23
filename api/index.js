a37f31efbfb6c5cc5a61eb2c3e785d730ccd7a2d
negar <negar.karimnejad@gmail.com>
2024-01-23 15:48:56 +0330

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log("Connected to MongoDB successfully👍");
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB successfully👍");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server listining on port 3000");
 });
 
-app.get("/api/user", userRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
