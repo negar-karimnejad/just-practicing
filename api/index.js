a66837bdf0b1a7d1b662d56af77ca224ff195064
negar <negar.karimnejad@gmail.com>
2024-01-23 13:33:21 +0330

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to MongoDB successfully👍");
+});
 
 const app = express();
 
